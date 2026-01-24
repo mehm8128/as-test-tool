2b2cb4ee13182985514912415542c7216703c745
mehm8128 <mehm8128@gmail.com>
2026-01-24 15:35:14 +0900

    fix settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		tanstackRouter({
-			target: 'react',
-			autoCodeSplitting: true
-		}),
-		react()
-	]
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true
+    }),
+    react()
+  ]
 })
