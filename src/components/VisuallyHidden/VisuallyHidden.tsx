d61cc681516c99ca3fe9e71055bd089046c4ce63
mehm8128 <mehm8128@gmail.com>
2026-01-29 21:27:47 +0900

    add visually hidden

---
+++ src/components/VisuallyHidden/VisuallyHidden.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from 'react'
+import styles from './VisuallyHidden.module.css'
+
+export function VisuallyHidden({ children }: { children: ReactNode }) {
+  return <span className={styles.module}>{children}</span>
+}
--- src/features/testDetail/components/SatisifedRadio.tsx
+++ src/features/testDetail/components/SatisifedRadio.tsx
@@ -1,5 +1,6 @@
 import { Check } from 'lucide-react'
 import styles from './SatisfiedRadio.module.css'
+import { VisuallyHidden } from '../../../components/VisuallyHidden/VisuallyHidden'
 
 export function SatisfiedRadio({
   id,
@@ -13,26 +14,30 @@ export function SatisfiedRadio({
   return (
     <fieldset className={styles.module} role="radiogroup" aria-labelledby={id}>
       <label className={styles.label}>
-        <input
-          type="radio"
-          name="isSatisfied"
-          value="satisfied"
-          className={styles.input}
-          onChange={() => onEditIsSatisfied(true)}
-          checked={isSatisfied === true}
-        />
+        <VisuallyHidden>
+          <input
+            type="radio"
+            name="isSatisfied"
+            value="satisfied"
+            className={styles.input}
+            onChange={() => onEditIsSatisfied(true)}
+            checked={isSatisfied === true}
+          />
+        </VisuallyHidden>
         {isSatisfied === true && <Check />}
         <span>満たしている</span>
       </label>
       <label className={styles.label}>
-        <input
-          type="radio"
-          name="isSatisfied"
-          value="notSatisfied"
-          className={styles.input}
-          onChange={() => onEditIsSatisfied(false)}
-          checked={isSatisfied === false}
-        />
+        <VisuallyHidden>
+          <input
+            type="radio"
+            name="isSatisfied"
+            value="notSatisfied"
+            className={styles.input}
+            onChange={() => onEditIsSatisfied(false)}
+            checked={isSatisfied === false}
+          />
+        </VisuallyHidden>
         {isSatisfied === false && <Check />}
         <span>満たしていない</span>
       </label>
