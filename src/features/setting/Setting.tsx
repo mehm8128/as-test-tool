26257301bc7592dbc5f1028070d59857dc62c9a8
mehm8128 <mehm8128@gmail.com>
2026-01-31 00:58:29 +0900

    設定画面にform要素を利用

--- src/features/setting/Setting.tsx
+++ src/features/setting/Setting.tsx
@@ -16,73 +16,75 @@ export function Setting() {
         <AnchorLink to="/">一覧へ</AnchorLink>
         <Heading level={1}>各種設定</Heading>
       </div>
-      <Label labelText="氏名">
-        <InputText
-          value={setting.name}
-          onChange={(value) =>
-            setSetting({
-              ...setting,
-              name: value
-            })
-          }
-        />
-      </Label>
-      <Label labelText="メールアドレス">
-        <InputText
-          type="email"
-          value={setting.email}
-          onChange={(value) =>
-            setSetting({
-              ...setting,
-              email: value
-            })
-          }
-        />
-      </Label>
-      <Label labelText="OS">
-        <InputText
-          value={setting.os}
-          onChange={(value) =>
-            setSetting({
-              ...setting,
-              os: value
-            })
-          }
-        />
-      </Label>
-      <Label labelText="ブラウザ">
-        <InputText
-          value={setting.browser}
-          onChange={(value) =>
-            setSetting({
-              ...setting,
-              browser: value
-            })
-          }
-        />
-      </Label>
-      <Label labelText="支援技術">
-        <InputText
-          value={setting.at}
-          onChange={(value) =>
-            setSetting({
-              ...setting,
-              at: value
-            })
-          }
-        />
-      </Label>
-      <Label labelText="支援技術に対する追加の設定">
-        <Textarea
-          value={setting.atSetting}
-          onChange={(value) =>
-            setSetting({
-              ...setting,
-              atSetting: value
-            })
-          }
-        />
-      </Label>
+      <form className={styles.form}>
+        <Label labelText="氏名">
+          <InputText
+            value={setting.name}
+            onChange={(value) =>
+              setSetting({
+                ...setting,
+                name: value
+              })
+            }
+          />
+        </Label>
+        <Label labelText="メールアドレス">
+          <InputText
+            type="email"
+            value={setting.email}
+            onChange={(value) =>
+              setSetting({
+                ...setting,
+                email: value
+              })
+            }
+          />
+        </Label>
+        <Label labelText="OS">
+          <InputText
+            value={setting.os}
+            onChange={(value) =>
+              setSetting({
+                ...setting,
+                os: value
+              })
+            }
+          />
+        </Label>
+        <Label labelText="ブラウザ">
+          <InputText
+            value={setting.browser}
+            onChange={(value) =>
+              setSetting({
+                ...setting,
+                browser: value
+              })
+            }
+          />
+        </Label>
+        <Label labelText="支援技術">
+          <InputText
+            value={setting.at}
+            onChange={(value) =>
+              setSetting({
+                ...setting,
+                at: value
+              })
+            }
+          />
+        </Label>
+        <Label labelText="支援技術に対する追加の設定">
+          <Textarea
+            value={setting.atSetting}
+            onChange={(value) =>
+              setSetting({
+                ...setting,
+                atSetting: value
+              })
+            }
+          />
+        </Label>
+      </form>
     </div>
   )
 }
