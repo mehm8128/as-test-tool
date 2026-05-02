5ab463f73cfbf6c78d1cb1bcf4e6e2865ff53a78
mehm8128 <mehm8128@gmail.com>
2026-05-02 13:55:08 +0900

    replace getByLabelText with getByRole

--- src/features/setting/Setting.test.tsx
+++ src/features/setting/Setting.test.tsx
@@ -1,10 +1,10 @@
+import { useAtom } from 'jotai'
+import { BrowserRouter } from 'react-router-dom'
 import { expect, test } from 'vitest'
 import { render, renderHook } from 'vitest-browser-react'
-import { Setting } from './Setting'
-import { useAtom } from 'jotai'
-import { settingAtom } from '../../states/setting'
-import { BrowserRouter } from 'react-router-dom'
 import { userEvent } from 'vitest/browser'
+import { settingAtom } from '../../states/setting'
+import { Setting } from './Setting'
 
 test('各フィールドに入力した情報がatomに保存される', async () => {
   // Arrange
@@ -15,12 +15,12 @@ test('各フィールドに入力した情報がatomに保存される', async (
   )
   const { result } = await renderHook(() => useAtom(settingAtom))
 
-  const nameInput = screen.getByLabelText('氏名')
-  const emailInput = screen.getByLabelText('メールアドレス')
-  const osInput = screen.getByLabelText('OS')
-  const browserInput = screen.getByLabelText('ブラウザ')
-  const atInput = screen.getByLabelText('支援技術')
-  const atSettingTextarea = screen.getByLabelText('支援技術に対する追加の設定')
+  const nameInput = screen.getByRole('textbox', { name: '氏名' })
+  const emailInput = screen.getByRole('textbox', { name: 'メールアドレス' })
+  const osInput = screen.getByRole('textbox', { name: 'OS' })
+  const browserInput = screen.getByRole('textbox', { name: 'ブラウザ' })
+  const atInput = screen.getByRole('textbox', { name: '支援技術' })
+  const atSettingTextarea = screen.getByRole('textbox', { name: '支援技術に対する追加の設定' })
 
   // Act
   await userEvent.fill(nameInput, 'taro yamada')
